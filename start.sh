01ee21f947d488b493acf1295682fffcada9feaa
Xiumie <y924996367@gmail.com>
2017-02-14 19:45:21 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 #       start code.py 0.0.0.0:8080  
 #       webpy proj
 # History:
-# 2017/02/02    YYb
+# 2017/02/02    Xiumie
 # PATH=/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 export PATH
